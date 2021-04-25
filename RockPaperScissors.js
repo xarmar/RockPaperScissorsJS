97a3885a7e73c0fe53125f4505bdefeec482000b
Xavier Marques <xavierpmarques@gmail.com>
2021-04-25 20:27:46 +0100

    Javascript for text based Rock Paper Scissors game.

---
+++ RockPaperScissors.js
@@ -0,0 +1,113 @@
+// START GAME
+let computerWins = 0;
+let playerWins = 0;
+game();
+
+function game() {
+    // KEEP TRACK OF WINS
+    // RUN 5 ROUNDS
+    for (round = 1; round<=5; round++) {
+        console.log("Round " + round + "!")
+        let computer = computerPlay();
+        let user = userPlay();
+        console.log("You picked " + user + ".")
+        console.log("Computer picked " + computer + ".")
+        play(computer, user);
+    }
+    // CHECK FOR WINNER
+    if (computerWins > playerWins) {
+        console.log("Computer WON!");
+        console.log(`Computer WON ${computerWins} round(s) out of 5.`);
+        console.log(`Player WON ${playerWins} round(s) out of 5.`);
+
+    }
+    else if (computerWins < playerWins) {
+        console.log("You WON!");
+        console.log(`Player WON ${playerWins} round(s) out of 5.`);
+        console.log(`Computer WON ${computerWins} round(s) out of 5.`);
+    }
+    else if (computerWins == playerWins) {
+        console.log("It's a DRAW!")
+    }
+}
+
+// PICK RANDOM COMPUTER CHOICE
+function computerPlay() {
+    let computerPick = Math.floor((Math.random() * 3));
+    if (computerPick === 0) {
+        let computerChoice = "Rock";
+        return computerChoice;
+    }
+    else if (computerPick === 1) {
+        let computerChoice = "Paper";
+        return computerChoice;
+    }
+    else if (computerPick === 2) {
+        let computerChoice = "Scissors";
+        return computerChoice;
+    }
+}
+
+
+
+// ASK USER FOR A CHOICE
+function userPlay() {
+    let userPick = prompt("Type your choice: 'Rock', 'Paper' or 'Scissors'.")
+    if (userPick.toUpperCase() === "ROCK") {
+        let playerChoice = "Rock";
+        return playerChoice;
+    }
+    else if (userPick.toUpperCase() === "PAPER") {    
+        let playerChoice = "Paper";
+        return playerChoice;
+    }
+    else if (userPick.toUpperCase() === "SCISSORS") {
+        let playerChoice = "Scissors";
+        return playerChoice;
+    }
+    else {
+        playerChoice = "Error";
+        return playerChoice;
+
+    }
+}
+
+function play(computerChoice, playerChoice) {
+    if (playerChoice === computerChoice) {
+        console.log("It's a draw!")
+    }
+    else {
+        console.log(playerChoice + " " + computerChoice);
+        let outcome;
+        switch (playerChoice + " " + computerChoice) {
+            case "Rock Paper":
+                console.log("You lose! Paper beats Rock!");
+                computerWins++;
+                break;
+            case "Rock Scissors":
+                console.log("You win! Rock beats Scissors!");
+                playerWins++;
+                break;
+            case "Paper Rock":
+                console.log("You win! Paper beats Rock!");
+                playerWins++;
+                break;
+            case "Paper Scissors":
+                console.log("You lose! Scissors beats Paper");
+                computerWins++;
+                break;
+            case "Scissors Rock":
+                console.log("You lose! Rock beats Scissors");
+                computerWins++;
+                break;
+            case "Scissors Paper":
+                console.log("You win! Scissors beats Paper");
+                playerWins++;
+                break;
+
+            default:
+                console.log("ERROR");
+        }
+    }
+    
+}
