bfaa7baf40e6c421220b9d827543680223a0cd60
Xavier Marques <xavierpmarques@gmail.com>
2021-05-07 23:25:59 +0100

    Add new classes in HTML, center elements with CSS and JS testing.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -50,7 +50,6 @@ function initiateRound(e) {
     let playerChoice = userPlay(userPick);
     let computerChoice = computerPlay();
     playRound(computerChoice, playerChoice);
-    feedbackContainer.classList.remove("opacityanimation");
     feedbackContainer.classList.add("opacityanimation");
     adversaryWins.innerText = computerWins;
     draws.innerText = draw;
@@ -105,36 +104,48 @@ function playRound(computerChoice, playerChoice) {
         console.log(playerChoice + " " + computerChoice);
         switch (playerChoice + " " + computerChoice) {
             case "Rock Paper":
+                feedbackContainer.classList.remove("opacityanimation");
+                console.log(feedbackContainer.classList.contains("opacityanimation"));
                 feedback.innerText = "Round " + roundNumber + " You lost! Paper beats Rock!";
                 computerWins++;
                 roundNumber++;
                 round.innerText = roundNumber;
                 break;
             case "Rock Scissors":
+                feedbackContainer.classList.remove("opacityanimation");
+                console.log(feedbackContainer.classList.contains("opacityanimation"))
                 feedback.innerText = "Round " + roundNumber + ": You won! Rock beats Scissors!";
                 playerWins++;
                 roundNumber++;
                 round.innerText = roundNumber;
                 break;
             case "Paper Rock":
+                feedbackContainer.classList.remove("opacityanimation");
+                console.log(feedbackContainer.classList.contains("opacityanimation"))
                 feedback.innerText = "Round " + roundNumber + ": You won! Paper beats Rock!";
                 playerWins++;
                 roundNumber++;
                 round.innerText = roundNumber;
                 break;
             case "Paper Scissors":
+                feedbackContainer.classList.remove("opacityanimation");
+                console.log(feedbackContainer.classList.contains("opacityanimation"))
                 feedback.innerText = "Round " + roundNumber + ": You lost! Scissors beats Paper";
                 computerWins++;
                 roundNumber++;
                 round.innerText = roundNumber;
                 break;
             case "Scissors Rock":
+                feedbackContainer.classList.remove("opacityanimation");
+                console.log(feedbackContainer.classList.contains("opacityanimation"))
                 feedback.innerText = "Round " + roundNumber + ": You lost! Rock beats Scissors";
                 computerWins++;
                 roundNumber++;
                 round.innerText = roundNumber;
                 break;
             case "Scissors Paper":
+                feedbackContainer.classList.remove("opacityanimation");
+                console.log(feedbackContainer.classList.contains("opacityanimation"))
                 feedback.innerText = "Round " + roundNumber +  ": You won! Scissors beats Paper";
                 playerWins++;
                 roundNumber++;
